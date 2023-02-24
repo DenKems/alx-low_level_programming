d64463e7be0cb4e68491c8dbd27ebb3f433b5046
Dennis Kemevor <kemevormdenis@gmail.com>
2023-02-24 06:24:44 -0800

    Prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
 * print_numbers - function that prints the numbers, from 0 to 9
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar('i');
 	}
 	_putchar("\n");
 }
