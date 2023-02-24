e3e3062d9c73916d9469de0fea223c669c898e1e
Dennis Kemevor <kemevormdenis@gmail.com>
2023-02-24 07:23:31 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,14 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 48; j < 62; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
 		}
 		_putchar('\n');
-		_putchar();
 	}
 }
