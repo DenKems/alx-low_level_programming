c52ffacdd317f1f788f689d3547ea4ab20b0f146
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-01 11:14:07 -0800

    Element of array in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* reverse_array - function that reverses the content of an array of integers
+* @a: pointer of array
+* @n: number of element of the array
+* Return: nothing
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	while (i = 0; i < n)
+	{
+		i++;
+	}
+	while (i >= 0; i--)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,4 +3,5 @@
 * Function that concatenates two strings. This only copying 'n' bytes from the second string.
 * Function that copies a string.
 * Function that compares two strings.
+* Functions that reverses the content of an array of integers.
 * 
