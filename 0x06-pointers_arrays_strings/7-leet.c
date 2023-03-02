c2d93606e756b7bc9811490fdfdfe2eee69661f7
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-02 09:34:07 -0800

    Letters of string to alphabets

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* leet - function  that encodes a string into 1337,
+* changes some letters in the string to numbers.
+* @abc: string
+* Return: return a character type string
+*/
+char *leet(char *abc)
+{
+	int i = 0;
+	int j = 0;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	while (abc[i] != '\0'; i++)
+	{
+		while (j < 10; j++)
+		{
+			if (abc[i] == str1[i])
+			{
+				abc[i] = str2[i];
+			}
+		}
+	}
+	return (abc)
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -6,4 +6,4 @@
 * Functions that reverses the content of an array of integers.
 * Function that changes all lowercase letters of a string to uppercase.
 * Function that capitalizes all words of a string.
-* 
+* Function that encodes a string into 1337. Changes some alphabets to numbers.
