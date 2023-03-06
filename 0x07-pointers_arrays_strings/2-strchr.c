aefd2b875b5521337245b0c3de9169f05c1c4e50
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-05 16:02:48 -0800

    Locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	while (i = 0; s != '\0')
+	while (s != '\0')
 	{
-		if (s[i] == c)
+		if (s == c)
 		{
-			return (s + i);
+			return (s);
 		}
-		i++;
+		s++;
 	}
 	return (NULL);
 }
