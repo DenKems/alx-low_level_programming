afa3c2057f8c2ed263ae35c632ebd062cdb4ec9e
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-10 17:18:37 -0800

    Number of arguments passed into a function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -function that prints the number of arguments passed
+ * followed by a new line
+ * @argc: number of arguments
+ * @argv: array of argumes
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	void(argv); /* ignore argv */
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
