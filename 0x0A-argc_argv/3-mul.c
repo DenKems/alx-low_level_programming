674cfe87e33915044070de70690505a4a261d65c
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-10 18:22:20 -0800

    Multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
+		num1 = _atoi(argv[1]);
+		num2 = _atoi(argv[2]);
 		printf("%d\n", num1 * num2);
 		return (0);
 	}
