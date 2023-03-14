99e878bc132ef0d3b689ce2d4c8c3669e4c18179
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-14 10:45:19 -0700

    Concatenate arguments to function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* argstostr - function that concatenates all the arguments of your program
+* @ac: integer for argument count
+* @av: double pointer of a string array
+* Return: returns pointer to a new string, or NULL if it fails
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, j, k;
+	int a = 0;
+	char *allconcat;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j])
+		{
+			j++;
+		}
+	}
+	j += ac;
+
+	allconcat = malloc(sizeof(char) * j + 1);
+	if (allconcat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (k = 0; av[i][k]; k++)
+		{
+			allconcat[a] = av[i][k];
+			a++;
+		}
+	}
+	if (allconcat[a] == '\0')
+	{
+		allconcat[a++] = '\n';
+	}
+	return (allconcat);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,3 +4,5 @@
 * Function that concatenates two strings
 * Function that returns a pointer to a 2 dimensional array of integers
 * Function that frees a 2 dimensional grid previously created by your alloc_grid function
+* Function that concatenates all the arguments of your program
+* Function that splits a string into words
