5dc18d04176a524837244c816cb609d1efe3f344
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-20 18:50:33 -0700

    Prints 'struct dog' type

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* print_dog - function prints a 'struct dog' type
+* @d: pointer to 'struck dog' variable to print
+* Return: reurns nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d.name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+	{
+		printf("Name: %s", &d.name);
+	}
+
+	if (d.age == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("Age: %f", &d.age);
+	}
+
+	if (d.owner == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("Owner: %s", &d.owner);
+	}
+}
