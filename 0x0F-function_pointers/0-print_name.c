f15d66530b394dfa0d2e2d5ae3514fa5d36b273d
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-22 07:51:52 -0700

    Prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/*
+* print_name - prints a name using callback
+* @name: name to be printed
+* @f: function pointer
+* Return: returns nothing
+*/
+void print_name(char *name, void (*f)(char *)) /* function prototype */
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
