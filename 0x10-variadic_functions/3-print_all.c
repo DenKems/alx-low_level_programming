be517d3b037224492f6ac98d87329c01464d2e11
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-23 17:19:45 -0700

    Prints strings

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,8 +11,8 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i;
 	char *str;
+	unsigned int i;
 
 	va_start(list, n);
 
@@ -28,6 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
+
 	printf("\n");
 
 	va_end(list);
