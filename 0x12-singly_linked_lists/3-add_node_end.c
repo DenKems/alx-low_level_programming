7a660330309c7265e4852d4f5f70911c0d89a128
Dennis Kemevor <kemevormdenis@gmail.com>
2023-03-30 19:22:34 -0700

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,8 +20,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	temp->next = NULL;
 
 	if (*head == NULL)
+	{
+		*head = temp;
 		return (temp);
-
+	}
 	while (ptr->next != NULL)
 		ptr = ptr->next;
 
