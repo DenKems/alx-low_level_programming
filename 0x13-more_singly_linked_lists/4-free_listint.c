0e6b04008e36979c4b6393d9a102fa04ba1c0461
Dennis Kemevor <kemevormdenis@gmail.com>
2023-04-03 16:40:45 -0700

    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,12 @@
 */
 void free_listint(listint_t *head)
 {
-	while (head != NULL)
-	{
-		free(head->next);
-	}
+	listint_t *temp;
 
-	free(head);
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
