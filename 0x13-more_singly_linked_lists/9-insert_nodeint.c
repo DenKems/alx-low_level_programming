b6eaa48a69bc8ae9c911c6f2b55ba4847bfe33a8
Dennis Kemevor <kemevormdenis@gmail.com>
2023-04-04 19:21:33 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 
-	for (i = 1; ptr && i < idx; i++)
+	for (i = 0; ptr && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
