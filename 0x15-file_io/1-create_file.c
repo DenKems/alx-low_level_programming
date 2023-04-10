9665d58d2c8517e132628a39b613345ddf529320
Dennis Kemevor <kemevormdenis@gmail.com>
2023-04-10 15:53:10 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
 * @filename: name of file
 * @text_content: content to write in the file
 *
-* Return: 1(Success) or -1(Failure)
+* Return: returns 1(Success) or -1(Failure)
 */
 int create_file(const char *filename, char *text_content)
 {
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fildes == -1)
 		return (-1);
 
