497341845eebd815d4909b92d49331b3c9c1f354
Dennis Kemevor <kemevormdenis@gmail.com>
2023-04-09 18:09:50 -0700

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* create_file - function creates a file
+* @filename: name of file
+* @text_content: content to write in the file
+* Return: 1(Success) or -1(Failure)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fildes, i, count;
+
+	if (!filename)
+		return (-1);
+
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fildes == -1)
+		return (-1);
+
+	i = 0;
+	while (text_content[i])
+	{
+		i++;
+	}
+
+	if (!text_content)
+		text_content = "";
+
+	count = write(fildes, text_content, i);
+	if (count == -1)
+		return (-1);
+
+	close(fildes);
+
+	return (1);
+}
