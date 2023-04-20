e6409ca5ed796f4e05756ed6c00598ea6d601a0e
Dennis Kemevor <kemevormdenis@gmail.com>
2023-04-19 18:02:25 -0700

    Prints 'n' Timetable

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+* print_times_table - function prints the n times table, starting with 0
+* @n: the nth type of timetable to print
+* Return: returns nothing
+*/
+void print_times_table(int n)
+{
+	int i, j, num;
+
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			num = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				if (num < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(num + '0');
+				}
+				else if (num >= 10 && num < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(num / 10 + '0');
+					_putchar(num % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(num / 100 + '0');
+					_putchar((num % 100) / 10 + '0');
+					_putchar((num % 100) % 10 + '0');
+				}
+				if (j == n)
+					_putchar('\n');
+			}
+			else
+				_putchar(num + '0');
+		}
+	}
+}
+
